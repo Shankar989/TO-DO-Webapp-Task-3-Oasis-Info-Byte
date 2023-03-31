e8f6afe63c45862ad5f6f81473617dde5e2ed548
Shankar989 <96803519+Shankar989@users.noreply.github.com>
2023-03-31 14:50:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {useState,useEffect} from "react";
+import "./App.css"
+import Header from "./components/Header";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+function App() {
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos));
+  },[todos]);
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+          <Form 
+          input={input}
+          setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+          editTodo={editTodo}
+          setEditTodo={setEditTodo}
+          />
+        </div>
+        <div>
+          <TodoList todos={todos} setTodos={setTodos} setEditTodo ={setEditTodo}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+
+
